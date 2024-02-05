e6a95a3852996d3f29b8d68ee4710133c89053d5
Jose Manuel <prueba@gmail.com>
2024-02-05 18:36:22 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template lang="">
+<h1>Listado de tareas</h1>
+<p>Se han encontrado {{tasks.length}} tareas</p>
+<p>{{ textoNumeroTareas }}</p>
+<ul>
+  <li v-for="task of tasks" :key="task.id">{{task.id}} - {{task.name}}</li>
+</ul>
+
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const tasks = ref([
+  {
+    id: 1,
+    name : 'Corregir examenes del lado del servidor.'
+  },
+  {
+    id: 2,
+    name : 'Corregir examenes del lado del cliente.'
+  },
+  {
+    id: 3,
+    name : 'Corregir examenes de recuperacion.'
+  },
+  {
+    id: 4,
+    name : 'Preparar examen de Vue'
+  },
+]);
+
+const textoNumeroTareas = computed(() => {
+if (tasks.value.length==0) return "No hay tareas";
+else if (tasks.value.length==1) return "Hay una tarea";
+return `Hay ${tasks.value.length} tareas`;
+});
+
+</script>
+<style lang="">
+  
+</style>
