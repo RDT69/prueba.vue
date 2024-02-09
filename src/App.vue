43ebddf99687e91d55ce4537912f83f9e7164578
Jose Manuel <prueba@gmail.com>
2024-02-09 18:11:08 +0000

    Vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template lang="">
+  <p>
+    Option: 
+    <select v-model="options">
+      <option value="list">Listado</option>
+      <option value="create">Crear tarea</option>
+    </select>
+  </p>
+
+<div v-if="options=='list'">
 <h1>Listado de tareas</h1>
+<p>Filtro
+  <input type="text" placeholder="Filtro" v-model="wordSerch">
+</p>
+<p>Nueva tarea: {{newTask}}</p>
 <p>Se han encontrado {{tasks.length}} tareas</p>
 <p>{{ textoNumeroTareas }}</p>
-<ul>
-  <li v-for="task of tasks" :key="task.id">{{task.id}} - {{task.name}}</li>
-</ul>
 
+<!-- Codigo de esta parte en el "ViewTask.vue" -->
+<ul>
+  <ViewTask
+  v-for ="task of filteredTask"
+  :key="task.id"
+  :task="task"
+  @remove-task="removeTask"
+  />
+</ul>
+<!-- Se ayuda de las funciones del script de este codigo. "removeTask" -->
+
+<ul>
+  <li v-for="task of tasks" 
+    :key="task.id"
+    @click="removeTask(task.id)">
+    {{task.id}} - {{task.name}}
+  </li>
+</ul>
+</div>
+
+<!-- Codigo de este parte en el "CreateTask.vue" -->
+<CreateTask 
+v-else 
+@insert-Task="insertTask"/> 
+<!-- Se ayuda de las funciones del script de este codigo. "insertTask"-->
 </template>
 
 <script setup>
 import { computed, ref } from 'vue';
+import CreateTask from './components/CreateTask.vue';
+import ViewTask from './components/ViewTask.vue';
 
+const options = ref('list');
 const tasks = ref([
   {
     id: 1,
@@ -29,12 +67,37 @@ const tasks = ref([
     name : 'Preparar examen de Vue'
   },
 ]);
-
+const wordSerch = ref('');
 const textoNumeroTareas = computed(() => {
 if (tasks.value.length==0) return "No hay tareas";
 else if (tasks.value.length==1) return "Hay una tarea";
 return `Hay ${tasks.value.length} tareas`;
 });
+
+const newTask = ref('');
+
+function insertTask(task) {
+  console.log('Insertando......');
+  console.log(task);
+  tasks.value.push({
+    id: newId.value,
+    name: task
+  });
+}
+
+const newId = computed(() =>{
+const max = Math.max(...tasks.value.map(t => t.id)) + 1;
+return max < 0 ? 1: max;
+});
+
+function removeTask(id) {
+  console.log('Eliminando...' + id);
+  tasks.value = tasks.value.filter(t => t.id != id);
+}
+
+const v = computed(()=>{
+  return tasks.value.filter(t => t.name.toLocaleLowerCase().includes(wordSerch.value.toLocaleLowerCase()));
+}); 
 
 </script>
 <style lang="">
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>
+    <input type="text" placeholder="Nueva Tarea" @keyup.enter="insertarTask" v-model="newTask">
+  </p>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const newTask = ref('');
+const emit = defineEmits(['insertTask']);
+
+function insertarTask(){
+  console.log('Insertando...' + newTask.value);
+  if (newTask.value.length>2) {
+    emit('insertTask', newTask.value);
+    newTask.value = '';
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    background-color: aquamarine;
+    padding: 10px;
+  }
+</style>
